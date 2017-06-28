5e2ec3f73f854a326a50e24fe0fecf83a6427357
Sudhir.Nayak <Sudhir.Nayak@WLW7-SUDHIRN.INDECOMM.LOCAL>
2017-06-28 08:21:05 +0530

    Session3 Assignment2

---
+++ src/session3/Assignment2.java
@@ -0,0 +1,14 @@
+package session3;
+
+public class Assignment2 {
+	int x; int y;
+	String name;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Assignment2 Obj = new Assignment2();
+		System.out.println ("Obj is " + Obj.name + " " + Obj.x + " " + Obj.y);
+		
+	}
+
+}
