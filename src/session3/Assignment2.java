49fc96ae8c8d4b169c8e330e6b9d70c85acf5db9
Sudhir.Nayak <Sudhir.Nayak@WLW7-SUDHIRN.INDECOMM.LOCAL>
2017-06-28 23:21:30 +0530

    Made changes

--- src/session3/Assignment2.java
+++ src/session3/Assignment2.java
@@ -1,14 +1,13 @@
 package session3;
 
 public class Assignment2 {
+
 	int x; int y;
 	String name;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Assignment2 Obj = new Assignment2();
-		System.out.println ("Obj is " + Obj.name + " " + Obj.x + " " + Obj.y);
+	
+	public static void main(String args[]) {
 		
+		Assignment2 pnt = new Assignment2();
+		System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
 	}
-
 }
